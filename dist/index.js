68bec1d848c76fdd373651bb97367cb31442e6c8
Alejandro D'Amen <alejandro.damen@educacionit.com>
2024-02-22 17:24:39 -0300

    New Version Release

--- dist/index.js
+++ dist/index.js
@@ -24740,7 +24740,11 @@ const run = async () => {
     const discordWebhook = core.getInput('discord-webhook', { required: true });
     if (!discordWebhook)
         throw new Error("discordWebhook doesn't exist");
-    core.info(`------>${discordWebhook}`);
+    const webhookParts = discordWebhook.split('://');
+    const protocol = webhookParts[0];
+    const remainingUrl = webhookParts[1];
+    core.info(`Protocol: ${protocol}`);
+    core.info(`URL: ${remainingUrl}`);
     // let res;
     // try {
     //   res = await axios.post(discordWebhook, 'hola');
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ export const run = async () => {
   const discordWebhook = core.getInput('discord-webhook', { required: true });
   if (!discordWebhook) throw new Error("discordWebhook doesn't exist");
 
-  core.info(`------>${discordWebhook}`);
+  const webhookParts = discordWebhook.split('://');
+  const protocol = webhookParts[0];
+  const remainingUrl = webhookParts[1];
+
+  core.info(`Protocol: ${protocol}`);
+  core.info(`URL: ${remainingUrl}`);
   // let res;
   // try {
   //   res = await axios.post(discordWebhook, 'hola');
