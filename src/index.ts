627dd0ca4fb6cd90f19cbcba476e0debd8013ca5
Alejandro D'Amen <alejandro.damen@educacionit.com>
2024-02-22 16:39:23 -0300

    New Version Release

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import * as core from '@actions/core';
 export const run = async () => {
   const discordWebhook = core.getInput('discord-webhook', { required: true });
   if (!discordWebhook) throw new Error("discordWebhook doesn't exist")
-  //testing
   core.info(`setting output LATEST_LAYER_VERSION: ${JSON.stringify(discordWebhook, null)} *** ${discordWebhook}`);
 };
 
