7a09112c4445510bc3b7f9dc808a0e714a7af8ac
Alejandro D'Amen <alejandro.damen@educacionit.com>
2024-02-22 17:19:42 -0300

    New Version Release

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ export const run = async () => {
   const discordWebhook = core.getInput('discord-webhook', { required: true });
   if (!discordWebhook) throw new Error("discordWebhook doesn't exist");
 
-  let res;
-  try {
-    res = await axios.post(discordWebhook, 'hola');
-    core.info(res.statusText);
-  } catch (err: any) {
-    throw new Error(err.message);
-  }
+  core.info(`------>${discordWebhook}`);
+  // let res;
+  // try {
+  //   res = await axios.post(discordWebhook, 'hola');
+  //   core.info(res.statusText);
+  // } catch (err: any) {
+  //   throw new Error(err.message);
+  // }
 };
 
 export default run;
